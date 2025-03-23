72b9b9505aa65010243915308d53d1ede15e116f
Pablo Dias <pablodias147@gmail.com>
2025-03-23 14:12:09 -0300

    primeiro

---
+++ script.js
@@ -0,0 +1,10 @@
+const n1 = document.getElementById('n1');
+const n2 = document.getElementById('n2');
+const sortear = document.getElementById('sortear');
+
+sortear.addEventListener('click', () => {
+    const min = parseInt(n1.value);
+    const max = parseInt(n2.value);
+    const random = Math.floor(Math.random() * (max - min + 1)) + min;
+    document.getElementById('res').innerText = random;
+});
