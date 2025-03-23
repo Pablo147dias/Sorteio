57be07eeef13e23ebdb8db0e4c289da1264799ee
Pablo Dias <pablodias147@gmail.com>
2025-03-23 14:54:17 -0300

    sorteio 1.1 pronto

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const n1 = document.getElementById('n1');
 const n2 = document.getElementById('n2');
 const sortear = document.getElementById('sortear');
 
-sortear.addEventListener('click', () => {
+sortear.addEventListener('click', (event) => {
     const min = parseInt(n1.value);
     const max = parseInt(n2.value);
     const random = Math.floor(Math.random() * (max - min + 1)) + min;
     document.getElementById('res').innerText = random;
+    event.preventDefault();
 });
